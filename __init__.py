5a651f050c01720d2365636b2a945b09d186de69
Thomas Sweeney <tsweeney256@gmail.com>
2018-01-17 16:12:21 -0500

    load status_id map at app startup

--- __init__.py
+++ __init__.py
@@ -1,10 +1,19 @@
 import os
 from flask import Flask
+from file_host.helpers import get_db_connection
 
 
 def create_app(settings_file):
     app = Flask(__name__)
     app.config.from_pyfile(settings_file)
+    site_user_statuses = {}
+    with app.app_context():
+        with get_db_connection() as db:
+            cursor = db.cursor()
+            cursor.execute('select status_id, status from site_user_status;')
+            for id, name in cursor:
+                site_user_statuses[name] = id
+    app.config['site_user_statuses'] = site_user_statuses
     import file_host.blueprints.index.views as index_views
     app.register_blueprint(index_views.blueprint)
     import file_host.blueprints.user.views as user_views
